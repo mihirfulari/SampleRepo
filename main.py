45f4712b141ac901fb858980714ec217ac3a34bb
Mihirrr02 <63767865+mihirfulari@users.noreply.github.com>
2025-03-26 15:50:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,27 @@ import time
 led = machine.Pin(2, machine.Pin.OUT)  # Onboard LED is usually on GPIO2
 
 while True:
-    led.value(not led.value())  # Toggle LED state
+    led.value(1)  # Toggle LED state
     time.sleep(0.1)  # Wait for 500ms
+    led.value(0)  # Toggle LED state
+    time.sleep(0.1)  # Wait for 500ms
+    led.value(1)  # Toggle LED state
+    time.sleep(0.1)  # Wait for 500ms
+    led.value(0)  # Toggle LED state
+    time.sleep(0.1)  # Wait for 500ms
+    led.value(1)  # Toggle LED state
+    time.sleep(0.1)  # Wait for 500ms
+    led.value(0)  # Toggle LED state
+    time.sleep(0.1)  # Wait for 500ms
+    
+    led.value(1)  # Toggle LED state
+    time.sleep(0.5)  # Wait for 500ms
+    led.value(0)  # Toggle LED state
+    time.sleep(0.5)  # Wait for 500ms
+    led.value(1)  # Toggle LED state
+    time.sleep(0.5)  # Wait for 500ms
+    led.value(1)  # Toggle LED state
+    time.sleep(0.5)  # Wait for 500ms
+    led.value(1)  # Toggle LED state
+    time.sleep(0.5)  # Wait for 500ms
+    led.value(0)  # Toggle LED state
