53b0bb7cb67e7e50e614863907a121529cc2978d
Mihirrr02 <63767865+mihirfulari@users.noreply.github.com>
2025-03-26 15:05:31 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,8 @@
+import machine
+import time
+
+led = machine.Pin(2, machine.Pin.OUT)  # Onboard LED is usually on GPIO2
+
+while True:
+    led.value(not led.value())  # Toggle LED state
+    time.sleep(0.5)  # Wait for 500ms
