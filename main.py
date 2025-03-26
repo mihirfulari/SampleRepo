be23f26d8ba34b48ade87991f7d0e0096fe76b81
Mihirrr02 <63767865+mihirfulari@users.noreply.github.com>
2025-03-26 16:37:16 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,24 @@
-import machine
-import time
+from machine import Pin, SPI
+import pcd8544_fb
+from ota import OTAUpdater
+from time import sleep
 
-led = machine.Pin(2, machine.Pin.OUT)  # Onboard LED is usually on GPIO2
+# Initialize SPI (HSPI) interface
+spi = SPI(1, baudrate=1000000, polarity=0, phase=0)
 
-while True:
-    led.value(1)  # Toggle LED state
-    time.sleep(0.1)  # Wait for 500ms
-    led.value(0)  # Toggle LED state
-    time.sleep(0.1)  # Wait for 500ms
-    led.value(1)  # Toggle LED state
-    time.sleep(0.1)  # Wait for 500ms
-    led.value(0)  # Toggle LED state
-    time.sleep(0.1)  # Wait for 500ms
-    led.value(1)  # Toggle LED state
-    time.sleep(0.1)  # Wait for 500ms
-    led.value(0)  # Toggle LED state
-    time.sleep(0.1)  # Wait for 500ms
-    
-    led.value(1)  # Toggle LED state
-    time.sleep(0.5)  # Wait for 500ms
-    led.value(0)  # Toggle LED state
-    time.sleep(0.5)  # Wait for 500ms
-    led.value(1)  # Toggle LED state
-    time.sleep(0.5)  # Wait for 500ms
-    led.value(0)  # Toggle LED state
-    time.sleep(0.5)  # Wait for 500ms
-    led.value(1)  # Toggle LED state
-    time.sleep(0.5)  # Wait for 500ms
-    led.value(0)  # Toggle LED state
+# Define control pins
+cs = Pin(2, Pin.OUT)   # Chip Select (CE)
+dc = Pin(15, Pin.OUT)  # Data/Command (DC)
+rst = Pin(0, Pin.OUT)  # Reset (RST)
+bl = Pin(12, Pin.OUT)  # Backlight (BL), optional
+
+# Initialize the LCD
+lcd = pcd8544_fb.PCD8544_FB(spi, cs, dc, rst)
+
+# Clear the display
+lcd.fill(0)
+
+# Display text
+lcd.text("-----------", 0, 0)
+lcd.text("<3",0,10)
+lcd.show()
